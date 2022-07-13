87dd59d6689b1011bdea500254fa0ebbdea1fdb9
Vladyslav <mk.vlad2003@gmail.com>
2022-07-13 19:11:41 +0300

    Do work with assertions

---
+++ cypress/e2e/test/test3.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="Cypress"/>
+
+it('SHOULD', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100)
+        .should('have.value', 100)
+        .and('be.visible')
+})
+
+it('EXPECT', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100).then(input=> {
+            expect(input).to.have.value(100)
+        })
+})
+
+it('Check values for Deposit', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+        .should('be.checked')
+    cy.get('input[type="radio"]').check('840', {force: true})
+        .should('be.checked')
+})
+
+it('check is visible archive link', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.contains('Мої депозити')
+        .trigger('mouseover')
+        .get('#archiveDeposits')
+        .should('be.visible')
+})
+
+it('check is correct attr in button', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+        .should('have.attr', 'type')
+        .and('match', /button/)
+})
+
+it.only('check is correct URL', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+        .should('eq', 'https://next.privat24.ua/?lang=en')
+})
